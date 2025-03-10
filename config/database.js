0c9c3e9ba0ee7876d063332000a9b8e9ed6e87a5
Nirmal Chandra Hota <raju.mahato@utkallabs.com>
2025-03-11 03:36:00 +0530

    123

--- config/database.js
+++ config/database.js
@@ -3,14 +3,18 @@ const { Sequelize } = require("sequelize");
 const env = process.env.NODE_ENV || "development";
 const config = require("./config");
 console.log("config : ", config[env]);
-const sequelize = new Sequelize(config[env], {
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // Bypass self-signed certificate error
+const sequelize = new Sequelize(
+  "postgres://amit_123:Tarique99471@amit-community-pg-db.c36uuosi88ze.ap-south-1.rds.amazonaws.com:5432/community",
+  {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
-  },
-  logging: console.log,
-});
+    logging: console.log, // Optional: Logs SQL queries to help debug
+  }
+);
 
 module.exports = sequelize;
